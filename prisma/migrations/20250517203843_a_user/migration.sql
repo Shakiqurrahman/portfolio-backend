86b379afb2bc2bf53a7c0a9c95a9e07db98959fe
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-05-18 02:55:17 +0600

    prisma setup done

---
+++ prisma/migrations/20250517203843_a_user/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "User_Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "role" "User_Role" NOT NULL DEFAULT 'USER',
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,16 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { authRoutes } from '../modules/auth/authRoute';
+import { userRoutes } from '../modules/user/userRoute';
 
 const router = Router();
 
 const moduleRoutes = [
-  {
-    path: "/auth",
-    route: AuthRoutes,
-  },
-  {
-    path: "/users",
-    route: userRoutes,
-  },
+    {
+        path: '/auth',
+        route: authRoutes,
+    },
+    {
+        path: '/users',
+        route: userRoutes,
+    },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
