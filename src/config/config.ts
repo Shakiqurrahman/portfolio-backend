e8a8d371fda83482090f9d3922e823dc39d1e7a6
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-05-27 22:35:47 +0600

    config updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -23,7 +23,10 @@ export const config = {
 };
 
 export const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+        'https://my-portfolio-shakiqurrahmans-projects.vercel.app',
+        'https://portfolio-dashboard-six-lilac.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
